d14fcfe3b74569444061adbeba19bf47892aaf29
Joanna Musing <joannamusing@gmail.com>
2022-05-17 23:36:52 -0500

    Initial setup.

---
+++ src/main/java/io/github/joannamusing/kazanjima/Main.java
@@ -0,0 +1,15 @@
+package io.github.joannamusing.kazanjima;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
