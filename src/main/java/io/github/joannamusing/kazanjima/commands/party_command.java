f27ee959318971e7144de7d798d3d3bf5a07720f
joannamusing <105577147+joannamusing@users.noreply.github.com>
2022-05-25 17:30:25 -0500

    Update party_command.java

--- src/main/java/io/github/joannamusing/kazanjima/commands/party_command.java
+++ src/main/java/io/github/joannamusing/kazanjima/commands/party_command.java
@@ -5,6 +5,8 @@ import com.velocitypowered.api.command.SimpleCommand;
 import net.kyori.adventure.text.Component;
 
 public class party_command implements CommandExecutor {
+
+TODO: Add in the "/party set" branch to allow for leader and name to be changed.
     
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -15,27 +17,33 @@ public class party_command implements CommandExecutor {
                 if(args.length > 0){
                     for(Player target : Bukkit.getOnlinePlayers()){
                         if(args[0].equalsIgnoreCase(target.getName())){
-                            //Creates a party if they are not in one, then invites the target player.
-                            //"/party <Player>"
+                            /*
+                            Creates a party if they are not in one, then invites the target player.
+                            /party <Player>
+                            */
                             if(p != null){
                                 Bukkit.dispatchCommand(player, "party invite " + target.getName();
                             }else{
-                                Bukkit.dispatchCommand(player, "party create " + (player.getName() + "'s Party);
+                                Bukkit.dispatchCommand(player, "party create " + player.getName();
                                 Bukkit.dispatchCommand(player, "party invite " + target.getName();
                             }
                         }
                     }
                     //If there is some kind of argument after party, we go to the switch. "/party <argument>"
                     switch(args[0]){
-                        //Toggles the party chat on or off.
-                        //"/party chat"
+                        /*
+                        Toggles the party chat on or off.
+                        /party chat
+                        */
                         case("chat"):
                             if(p != null){
                                 PartyManager.togglePartyChat(player);
                             }
                             break;
-                        //Creates a new party withthe specified name.
-                        //"/party create <String>"
+                        /*
+                        Creates a new party with the specified name.
+                        /party create <String>
+                        */
                         case("create"):
                             if(p == null){
                                 if(args.length == 2){
@@ -47,8 +55,10 @@ public class party_command implements CommandExecutor {
                             }else{
                                 player.sendMessage("You are already in a party.");
                             break;
-                        //This will let the party leader disband the party.
-                        //"/party disband"
+                        /*
+                        This will let the party leader disband the party.
+                        /party disband
+                        */
                         case("disband"):
                             if(p != null){
                                 if(p.isLeader(player)){
@@ -56,8 +66,11 @@ public class party_command implements CommandExecutor {
                                 }
                             }
                             break;
+                        /*
+                        Gives the user a list of party commands.
+                        /party help
+                        */
                         case("help"):
-                            //Help command to be helpful. Gives them the options.
                             ArrayList<String> strings = new ArrayList<>();
                             strings.add("/party - Check your party status.");
                             strings.add("/party <Player> - Invites another player to join your party.");
@@ -107,11 +120,16 @@ public class party_command implements CommandExecutor {
                             }
                             break;
                     }
+                /*
+                This is the general party command. It will open the party GUI ifthey are in a party, otherwise return a message.
+                /party
+                */
                 }else{
                     if(p != null){
-                    }
-                    //This will be a basic "/party" command.
-                    //If the player is in a party, return the party GUI. Otherwise, return some statement or "/party help".
+                        party_gui pgui = new party_gui(player, p);
+                        player.openPartyGUI();
+                    }else{
+                        player.sendMessage("You are not in a party. Try \"/party help\".");
                 }
             }
         }
